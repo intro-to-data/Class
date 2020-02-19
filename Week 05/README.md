4b004f7569f256fd894eaeb52336af75ae03ad17
Andy Choens <andy.choens@acuitashealth.com>
2020-02-19 14:29:55 -0500

    Some updates
    Changes to be committed:
      new file:   Week 05/README.md
      renamed:    Week 05/functions.R -> Week 06/functions.R
      renamed:    Week 05/intro-to-relational-class.R -> Week 06/intro-to-relational-class.R
      renamed:    Week 05/intro-to-relational.R -> Week 06/intro-to-relational.R
      renamed:    Week 05/packages.R -> Week 06/packages.R

---
+++ Week 05/README.md
@@ -0,0 +1,3 @@
+I had a massive code failure, so I moved all of that code to Week 06 and I will have to rethink Week 05.
+
+What I did as a fallback was just to try to go through what he goes through in the book.
--- Week 06/intro-to-relational.R
+++ Week 06/intro-to-relational.R
@@ -4,19 +4,17 @@
 
 
 ## INIT ========================================================================
-library(knitr)
 library(tidyverse)
 library(DBI)
-source()
 
 
 
 ## DATA =======================================================================
-## Downloads the data file, connects to the database, opens all of the tables.
 download.file(url = "https://github.com/intro-to-data/Data/raw/master/chinook.db",
               destfile = "chinook.db")
 con <- dbConnect(RSQLite::SQLite(), dbname = "chinook.db")
-##dbListTables(con)
+dbListTables(con)
+
 Albums         <- dbReadTable(con, "albums") %>% as_tibble()
 Artists        <- dbReadTable(con, "artists") %>% as_tibble()
 Customers      <- dbReadTable(con, "customers") %>% as_tibble()
@@ -35,14 +33,24 @@ dbDisconnect(con)
 ## SINGLE TABLE TRANSFORMATIONS ================================================
 ## Each table has it's own grain. And these grains intersect and have
 ## relationships with each other.
-## We will start with tracks.
+## We will start with Tracks.
 
-## How many albums?
+## How many Albums?
+Tracks %>%
+    select(AlbumId) %>%
+    distinct() %>%
+    count()
 
 ## How many Tracks per album?
+Tracks %>%
+    group_by(AlbumId) %>%
+    summarize(Tracks = n())
 
-## How many albums have 10 or more Tracks?
-
+## How many Albums have 10 or more Tracks?
+Tracks %>%
+    group_by(AlbumId) %>%
+    summarize(Tracks = n()) %>%
+    filter(Tracks >= 10)
 
 ## These are all transformation we can do with a single table.  IRL,
 ## most data is stored in a relational data structure.  When you do a
@@ -62,17 +70,29 @@ dbDisconnect(con)
 ## Let's look at tracks from a single Album.
 ## Specifically, AlbumId == 3.
 ## Let me see all the rows.
-
+Tracks %>%
+    filter(AlbumId == 3) %>%
+    kable()
 
 ## Let's talk about Keys.
-
+## TrackId: Primary Key
+## AlbumId: Foreign Key
+## MediaTypeId: Foreign Key
+## GenreId: Foreign Key
 
 ## Now, what do you see in Albums where AlbumId is 3?
+## AlbumId in Tracks "maps" to AlbumId in albuns.
+## In Albums, AlbumId is a primary key.
+## Primary keys and foreign keys tend to connect!
+Albums %>%
+    filter(AlbumId == 3) %>%
+    kable()
 
 ## ArtistId in Albums is a foreign key in Artists.
 ## Get the artist where ArtistId is 2.
-
-
+Artists %>%
+    filter(ArtistId == 2) %>%
+    kable()
 
 ## The relationships between primary keys and foreign keys are often
 ## shown in a Entity Relationship Diagram (ERD). In an
@@ -82,7 +102,7 @@ dbDisconnect(con)
 
 
 
-## FIRST JOINS =================================================================
+## JOINS =======================================================================
 ## Traversing the ERD by looking into each table manually is . . . inefficient.
 ## So, we can COMBINE the tables.
 
@@ -96,23 +116,43 @@ Tracks %>%
     kable() %>%
     head(25)
 
-
 ## Join Playlists to PaylistsTracks
 ## Show how many tracks are in each playlist.
-
+Playlists %>%
+    inner_join(PaylistsTracks, by = "PlaylistId") %>%
+    group_by(PlaylistId, Name) %>%
+    count()
 
 ## What are the names of Playlists with 25 or more tracks?
 ## You can reuse your previous code.
+Playlists %>%
+    inner_join(PaylistsTracks, by = "PlaylistId") %>%
+    group_by(PlaylistId, Name) %>%
+    count() %>%
+    filter(n >= 25)
 
 ## What is the name of customer with the most Invoices?
+Customers %>%
+    inner_join(Invoices, by="CustomerId") %>%
+    group_by(FirstName, LastName) %>%
+    count() %>%
+    arrange(desc(n)) %>%
+    kable()
 
 
 ## What is the highest value Invoice?
+Invoices %>%
+    inner_join(InvoiceItems, by = "InvoiceId") %>%
+    group_by(InvoiceId) %>%
+    summarize(Value = sum(UnitPrice)) %>%
+    arrange(desc(Value))
 
 
 ## Who are the most valuable customers?
-
-
-
-## BONUS!!!
-## What songs did Steve Murray buy?
+Invoices %>%
+    inner_join(InvoiceItems, by = "InvoiceId") %>%
+    group_by(CustomerId) %>%
+    summarize(Value = sum(UnitPrice)) %>%
+    inner_join(Customers, by = "CustomerId") %>%
+    select(FirstName, LastName, Value) %>%
+    arrange(desc(Value))
